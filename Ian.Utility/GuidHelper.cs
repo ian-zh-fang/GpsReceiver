9a7d37b9d8b80b048672bc58daa91b4caa491bf1
ian <ian.zh.fang@gmail.com>
2015-03-30 09:26:44 +0800

    对Guid增加扩展方法（ToLong-将一个 Guid 的实例转换成Int64 类型的实例）

---
+++ Ian.Utility/GuidHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ian.Utility
+{
+    public static class GuidHelper
+    {
+        public static long ToLong(this Guid g)
+        {
+            byte[] buffer = g.ToByteArray();
+            return BitConverter.ToInt64(buffer, 0);
+        }
+    }
+}
